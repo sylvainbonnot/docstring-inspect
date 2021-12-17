fbdc6759783e9258853287294fad5c439233a414
sylvainbonnot <sppbonnot@gmail.com>
2021-12-17 18:29:54 +0100

    added samplemodule

--- ast_utils.py
+++ ast_utils.py
@@ -18,4 +18,10 @@ def simple_print(func_defs):
         print('---')
         print(ast.get_docstring(f))
 
-        
+
+
+if __name__ == '__main__':
+    fname = 'samplemodule.py'
+    file_contents = file_contents(fname)
+    funcs_defs = funcs_from_file_contents(file_contents)
+    simple_print(funcs_defs)
